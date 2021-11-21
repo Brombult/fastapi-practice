fc214d924c6009e334771a4d900cdfd8cb6ffc11
Borys Rombult <borysrombult@gmail.com>
2021-11-21 21:22:26 +0200

    docs

--- main.py
+++ main.py
@@ -4,7 +4,34 @@ from fastapi.responses import RedirectResponse
 
 from routers import auth, post, user, vote
 
-app = FastAPI()
+
+description = """
+### Posts
+You can create, read, update and delete posts.
+All action except for reading post require authentication.
+
+### Users
+You can create users and retrieve user data.
+
+### Vote
+You can vote for posts.
+"""
+tags_metadata = [
+    {
+        "name": "Posts",
+        "description": "CRUD operations with posts.",
+    },
+    {
+        "name": "Users",
+        "description": "Create and retrieve users.",
+    },
+    {
+        "name": "Vote",
+        "description": "Upvote and downvote posts (1 for upvote and 0 for downvote).",
+    },
+]
+
+app = FastAPI(title="Simple Blog Application", description=description, openapi_tags=tags_metadata)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
