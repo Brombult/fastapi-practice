a722b09fe299ae6b13e1cc474e2d502fa4d8b40e
Borys Rombult <borysrombult@gmail.com>
2021-11-21 12:50:10 +0200

    added Procfile

--- main.py
+++ main.py
@@ -23,7 +23,3 @@ app.add_middleware(
 @app.get("/")
 def index():
     return {"message": "Hello World"}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
